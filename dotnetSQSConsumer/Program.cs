17ef1c8201307b4f30ad2610cc4748ad979e8d83
Luiz-HSSD <lhenrique_diniz@hotmail.com>
2025-08-14 22:35:23 -0300

    Adicionar arquivos de projeto.

---
+++ dotnetSQSConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+AmazonSQSClient sqsClient = new AmazonSQSClient();
+string? queueUrl = Environment.GetEnvironmentVariable("UrlQueue");
+// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    // Receiving messages
+    var receiveMessageRequest = new ReceiveMessageRequest
+    {
+        QueueUrl = queueUrl,
+        MaxNumberOfMessages = 1
+    };
+    var receiveMessageResponse = await sqsClient.ReceiveMessageAsync(receiveMessageRequest);
+    if (receiveMessageResponse.Messages != null)
+    {
+        foreach (var message in receiveMessageResponse.Messages)
+        {
+            Console.WriteLine($"Received message: {message.Body}");
+
+            // Deleting the message after processing
+            var deleteMessageRequest = new DeleteMessageRequest
+            {
+                QueueUrl = queueUrl,
+                ReceiptHandle = message.ReceiptHandle
+            };
+            await sqsClient.DeleteMessageAsync(deleteMessageRequest);
+            Console.WriteLine("Message deleted.");
+        }
+    }
+    Thread.Sleep(500);
+}
+
---
+++ dotnetSQSPublisher/Controllers/SendMessageSQSController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace dotnetSQSPublisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendMessageSQSController : ControllerBase
+    {
+        AmazonSQSClient sqsClient = new AmazonSQSClient();
+        private string? queueUrl = Environment.GetEnvironmentVariable("UrlQueue");
+
+        private readonly ILogger<SendMessageSQSController> _logger;
+
+        public SendMessageSQSController(ILogger<SendMessageSQSController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<object> Post()
+        {
+            // Sending a message
+            var sendMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = queueUrl,
+                MessageBody = "Hello from C#!"
+            };
+            await sqsClient.SendMessageAsync(sendMessageRequest);
+            return new { message ="enviado con sucesso" };
+        }
+    }
+}
